2945a54efd19793bdd8ade3dae7569d9f307ad5d
Sandip Ransing <sandip.ransing@synerzip.com>
2016-05-24 16:51:41 +0530

    [ui] Navbar aligned to right side
    This also removes debugger added on itemContainer

--- web/src/js/containers/App.js
+++ web/src/js/containers/App.js
@@ -38,7 +38,7 @@ export default class CoreLayout extends React.Component {
         </div>
 
         <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-        <ul className="nav navbar-nav">
+        <ul className="nav navbar-nav navbar-right">
         <li>
         <Link  to="/">Home</Link>
         </li>
