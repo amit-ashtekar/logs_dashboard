1c877ff52f7e42c2ac2e6d3f1f92a775479478db
Sandip Ransing <sandip.ransing@synerzip.com>
2016-05-25 17:49:56 +0530

    Header & footer alignments fixed

--- web/src/js/containers/App.js
+++ web/src/js/containers/App.js
@@ -4,88 +4,56 @@ import {Link} from 'react-router';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {logoutAndRedirect} from 'common/actions';
-
-
-
-
-
 import '../../css/core.scss';
-
 
 export default class CoreLayout extends React.Component {
 
-    render () {
+  render () {
+    const {dispatch} = this.props;
 
-        const {dispatch} = this.props;
-
-        return (
-            <div>
-
-
-
+    return (
+      <div>
         <nav className="navbar navbar-default" role="navigation">
-        <div className="container">
+          <div className="container">
+            <div className="navbar-header">
+              <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span className="sr-only">Toggle navigation</span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+              </button>
+              <a className="navbar-brand" href="index.html">Business Casual</a>
+            </div>
 
-        <div className="navbar-header">
-        <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-        <span className="sr-only">Toggle navigation</span>
-        <span className="icon-bar"></span>
-        <span className="icon-bar"></span>
-        <span className="icon-bar"></span>
-        </button>
-
-        <a className="navbar-brand" href="index.html">Business Casual</a>
-        </div>
-
-        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-        <ul className="nav navbar-nav navbar-right">
-        <li>
-        <Link  to="/">Home</Link>
-        </li>
-        <li>
-        <Link  to="/itemcontainer">Dashboard</Link>
-        </li>
-
-        </ul>
-
-        </div>
-
-        </div>
-
+            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul className="nav navbar-nav navbar-right">
+                <li><Link  to="/">Home</Link></li>
+                <li><Link  to="/itemcontainer">Dashboard</Link></li>
+              </ul>
+            </div>
+          </div>
         </nav>
-
         <div className="container">
-        <div className='row'>
-
-        {this.props.children}
-
+          <div className='row'>
+            {this.props.children}
+          </div>
         </div>
-
-
-
-
-
-
-        </div>
-<footer>
-<div className="container">
-        <div className="row">
-        <div className="col-lg-12 text-center">
-        <p>Copyright &copy; Synerzip.com</p>
-</div>
-</div>
-</div>
-</footer>
-    </div>
-
-        );
-    }
+        <footer className="footer">
+          <div className="container">
+            <p className="text-muted">
+              &copy; 2016 SYNERZIP, all rights reserved.
+            </p>
+          </div>
+        </footer>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state) => ({
-    isAuthenticated   : state.login.isAuthenticated,
-    statusText         : state.login.message,
-    addeditemsCount:state.AddedItemsCount.length
+  isAuthenticated   : state.login.isAuthenticated,
+  statusText         : state.login.message,
+  addeditemsCount:state.AddedItemsCount.length
 });
 
 export default connect(mapStateToProps)(CoreLayout);
